f0cafc7841ee87ba359cfbe569f5fab382ba437d
Ibrahim Semary <92101864+ibrahimsemary@users.noreply.github.com>
2022-09-25 03:46:13 -0500

    made request func

--- frontend/src/pages/Request.js
+++ frontend/src/pages/Request.js
@@ -1,38 +1,54 @@
 import React, { useState } from "react";
 import InputField from "../components/InputField";
 import Clock from "../components/Clock";
+import { Checkbox } from "@mui/material";
+import { getFunctions, httpsCallable } from "firebase/functions";
 
 const Request = ({ logOut, userData, setSearchState }) => {
+    const label = { inputProps: { "aria-label": "Checkbox demo" } };
     const [fromInput, setFromInput] = useState("");
     const [toInput, setToInput] = useState("");
-    const [earliestTime, setEarliestTime] = useState("");
-    const [latestTime, setLatestTime] = useState("");
-    const [genderPref, setGenderPref] = useState("");
+    const [earliestTime, setEarliestTime] = useState(null);
+    const [latestTime, setLatestTime] = useState(null);
+    const [genderPref, setGenderPref] = useState(false);
 
-    const findGroup = () => {
-        if (earliestTime.valueOf < Date.now()) {
-            earliestTime.add(1, "day");
+    const findGroup = async () => {
+        let early;
+        let late;
+        if (earliestTime.valueOf() < Date.now()) {
+            early = earliestTime.valueOf() + 86400000;
+        } else {
+            early = earliestTime.valueOf();
         }
-        if (latestTime.valueOf() <= earliestTime.valueOf()) {
-            latestTime.add(1, "day");
+        if (latestTime.valueOf() <= early) {
+            late = latestTime.valueOf() + 86400000;
+            if (late <= early) {
+                late = late + 86400000;
+            }
+        } else {
+            late = latestTime.valueOf();
         }
 
-        console.log(earliestTime);
-        console.log(latestTime);
+        let temp1 = fromInput.replace(/ /g, "+");
 
-        let temp1 = fromInput.replace(/\s+/g, '');
-        setFromInput(temp1);
-
-        temp1  = toInput.replace(/\s+/g, '');
-        setToInput(temp1);
+        let temp2 = toInput.replace(/ /g, "+");
 
         const to_send = {
-            fromLocation: fromInput,
-            toLocation: toInput,
-            beginTime: earliestTime,
-            endTime: latestTime,
-            genderPref: 
+            startAddress: temp1,
+            endAddress: temp2,
+            startTime: early,
+            endTime: late,
+            genderPref: genderPref,
+        };
+        const functions = getFunctions();
+        const searchGroup = httpsCallable(functions, "findGroup");
 
+        try{
+            const groupId = await searchGroup(to_send);
+            console.log(groupId)
+        }
+        catch(e){
+            console.log(e)
         }
 
         //setSearchState("searching");
@@ -64,6 +80,13 @@ const Request = ({ logOut, userData, setSearchState }) => {
                     <div className='clock'>
                         <Clock label='Latest Time' setVal={setLatestTime} />
                     </div>
+                </div>
+                <div>
+                    <Checkbox
+                        {...label}
+                        onChange={() => setGenderPref(!genderPref)}
+                    />
+                    Find me a group with at least 1 person of my same gender
                 </div>
                 <br />
                 <div className='main-button'>
